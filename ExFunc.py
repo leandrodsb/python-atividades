3bfdbe661ff9b9b0a13a272bd8442c22432919ec
leandrodsb <leandrods_517@outlook.com>
2020-10-04 00:25:30 -0400

    adicionando codigo

---
+++ ExFunc.py
@@ -0,0 +1,24 @@
+#def soma(x,y): #def: comando que define um novo conceito, no caso uma função.
+ #   return x+y  #return: resultado da função, valor que será retornado
+# Não é preciso definir o tipo de retorno da função no python
+
+#print(soma(20,10))
+#print(soma(10,10))
+
+#é possível utilizar uma outra função como parametro
+
+#print(soma(20*3,soma(3,5)))
+
+#é possível que funções realizem apenas ações sem receber parametros
+
+#def nome_do_seu_time():
+#    return "MFC"
+
+#exemplo de funções que realizam ações, não recebem nem parametros e nem retornam valores:
+
+#def nome ():
+#    print("Nome")
+
+#nome()
+
+
